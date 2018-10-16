91a74a0c43cac2b67edb4769db1e09f1514bc425
Andres Buelvas <afroelectrico@gmail.com>
2018-10-16 16:18:33 -0400

    tablas lista

--- configuracion/views.py
+++ configuracion/views.py
@@ -7,19 +7,45 @@ from django.urls import reverse_lazy, reverse
 
 #Create your views here.
 def index(request):
-	
-	estudios_fisicos = Estudio.objects.get(tipo="FS")
-	estudios_biologicos = Estudio.objects.get(tipo="BIO")
-	estudios_socioculturales = Estudio.objects.get(tipo="SC")
+	estudios_fisicos = Estudio.objects.filter(tipo="FS")
+	estudios_biologicos = Estudio.objects.filter(tipo="BIO")
+	estudios_socioculturales = Estudio.objects.filter(tipo="SC")
 
-	return render(request, 'configuracion/index.html', {'estudios_fisico':estudios_fisicos, 'estudios_biologicos':estudios_biologicos, 'estudios_socioculturales':estudios_socioculturales})
+	cant_fisicos = estudios_fisicos.count()
+	cant_biologicos = estudios_biologicos.count()
+	cant_socioculturales = estudios_socioculturales.count()
+
+	maximo = max([cant_fisicos, cant_biologicos, cant_socioculturales])
+	lista = []
+	for i in range(0, maximo):
+		lista.append([])
+
+	for i in range(0, maximo):
+		if i < cant_fisicos:
+			lista[i].append(estudios_fisicos[i])
+		else:
+			lista[i].append(None)
+
+	for i in range(0, maximo):
+		if i < cant_biologicos:
+			lista[i].append(estudios_biologicos[i])
+		else:
+			lista[i].append(None)
+
+	print(lista)
+	for i in range(0, maximo):
+		if i < cant_socioculturales:
+			lista[i].append(estudios_socioculturales[i])
+		else:
+			lista[i].append(None)
+
+	return render(request, 'configuracion/index.html', {'lista':lista})
 
 # Formulario para registrar un estudio/impacto
 class EstudioCreate(CreateView):
 	model = Estudio
 	form_class = EstudioForm
 	template_name = 'configuracion/agregar_estudio.html'
-	# success_url = reverse_lazy('index')
 
 	def grado_pertubacion(self):
 		return GRADO_PERTUBACION
@@ -52,7 +78,6 @@ class EstudioUpdate(UpdateView):
 			return reverse('eliminar_estudio', args=(self.object.id,))
 
 def calcular_via(request, pk):
-	print("Este es el id creado"+str(pk))
 	estudio = Estudio.objects.get(id=pk)
 	via = estudio.intensidad*estudio.pondIntensidad + estudio.extension*estudio.pondExtension + estudio.duracion*estudio.pondDuracion + estudio.reversibilidad*estudio.pondReversibilidad + estudio.probabilidad*estudio.pondProbabilidad
 	estudio.via = via
@@ -62,7 +87,6 @@ def calcular_via(request, pk):
 
 def eliminar_estudio(request, pk):
 	estudio = Estudio.objects.get(id=pk).delete()
-	print("HO")
 	return HttpResponseRedirect(reverse('index'))
 
 
