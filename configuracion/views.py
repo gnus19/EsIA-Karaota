05ead0d06f16ac3593fcd5dda7b14da8f4438620
Andres Buelvas <afroelectrico@gmail.com>
2018-10-16 08:29:57 -0400

    id capturado create view

--- configuracion/views.py
+++ configuracion/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from configuracion.forms import *
 from configuracion.models import *
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 #Create your views here.
 def index(request):
@@ -28,7 +28,7 @@ class EstudioCreate(CreateView):
 	model = Estudio
 	form_class = EstudioForm
 	template_name = 'configuracion/agregar_estudio.html'
-	success_url = reverse_lazy('index')
+	# success_url = reverse_lazy('index')
 
 	def grado_pertubacion(self):
 		return GRADO_PERTUBACION
@@ -45,9 +45,13 @@ class EstudioCreate(CreateView):
 	def rev_clasificacion(self):
 		return REV_CLASIFICACION
 
-def calcular_via(request):
-	# print(request) 
-	return
+	def get_success_url(self):
+		return reverse('calcular_via', args=(self.object.id,))
+
+def calcular_via(request, pk):
+	print("Este es el id creado"+str(pk))
+
+	return HttpResponseRedirect(reverse('index'))
 
 class EstudioUpdate(UpdateView):
 	model = Estudio
