ef078555a0326efe3dc4f77a3c4bd43c99e18000
Andres Buelvas <afroelectrico@gmail.com>
2018-10-30 01:24:56 -0400

    funciona todos los forms de registro estudio, uppdate y eliminar otra vez

--- configuracion/urls.py
+++ configuracion/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
 	url(r'^index/$', views.index, name='index'),
 	url(r'^agregar_estudio/$', EstudioCreate.as_view(), name='agregar_estudio'),
-	# url(r'^editar_estudio/(?P<pk>\d+)/$', EstudioUpdate.as_view(), name='editar_estudio'),
-	# url(r'^eliminar_estudio/(?P<pk>\d+)/$', views.eliminar_estudio, name='eliminar_estudio'),
+	url(r'^editar_estudio/(?P<pk>\d+)/$', EstudioUpdate.as_view(), name='editar_estudio'),
+	url(r'^eliminar_estudio/(?P<pk>\d+)/$', views.eliminar_estudio, name='eliminar_estudio'),
 	url(r'^tablas/$', views.tablas, name='tablas'),
 	url(r'^modificar_tablas/$', views.modificar_tablas, name='modificar_tablas'),
 ]
